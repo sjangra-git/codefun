79ffbff1ae303dab25becccc309620161efc5a7f
Sandeep Jangra <sjangra@walmartlabs.com>
2016-07-28 13:01:07 -0700

    Pairs example

--- src/main/java/codefun/Solution1.java
+++ src/main/java/codefun/Solution1.java
@@ -12,51 +12,9 @@ public class Solution1 {
     
     public static void main(String[] args)
     {
-        ListNode n1 = new ListNode(1);
-        ListNode n2 = new ListNode(2);
-        n1.next = n2;
-        n2.next = null;
-        
-        System.out.println(isPalindrome(n1));
+    	String[] in = new String[] {"101", " 102", "103 "};
+    	List<String> list = Arrays.asList(in);
+
     }
     
-    public static boolean isPalindrome(ListNode head) {
-        if(head == null) return true;
-        
-        // count length;
-        ListNode curr = head;
-        int length=0;
-        while(curr != null)
-        {
-            length++;
-            curr = curr.next;
-        }
-        
-        if(length ==1) return true;
-        
-        // count hashes
-        long h1 = 0;
-        // reverse hash
-        long h2 = 0;
-        
-        curr = head;
-        int i= 0;
-        while(curr != null)
-        {            
-            h1 += curr.val * Math.pow(10, i);
-            h2 += curr.val * Math.pow(10, length - i - 1);
-            i++;
-            curr = curr.next;
-        }
-        
-        if(h1 == h2) return true;
-        else return false;
-    }
-    
-    public static class ListNode
-    {
-        int val;
-        ListNode next;
-        ListNode(int x) {val = x;}
-    }
 }
---
+++ src/main/java/codefun/hackerrank/Pairs.java
@@ -0,0 +1,49 @@
+package codefun.hackerrank;
+
+import java.util.*;
+
+import java.io.*;
+import java.util.*;
+
+public class Pairs {
+
+  public static void main(String[] args) 
+  {
+	  Scanner scanner = new Scanner(System.in);
+	  int N = scanner.nextInt();
+	  int K = scanner.nextInt();
+	  
+	  int[] a = new int[N];
+	  for(int i = 0; i<N; i++)
+	  {
+		  a[i] = scanner.nextInt();
+	  }
+	  
+	  System.out.println(bruteForce(N, K, a));
+  }
+  
+  public static int bruteForce(int N, int K, int[] a)
+  {
+	  int count = 0;
+	  
+    	  Arrays.sort(a);
+
+	  for(int i = 0; i<N-1; i++)
+	  {
+		  for (int j = i+1; j<N; j++)
+		  {
+              
+			  if(Double.compare(K, Math.abs(a[i] - a[j])) == 0)
+			  {
+				  count++;
+                  break;
+			  }
+              else if(Double.compare(K, Math.abs(a[i] - a[j])) < 0) {
+                  break;
+              }
+		  }
+	  }
+	  
+	  return count;
+  }
+}
